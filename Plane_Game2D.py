44544a059e3310375b78213f3e6c8301d4ff9800
littleblack111 <jerrylam110209@gmail.com>
2023-03-24 13:35:41 +0800

    update

--- Plane_Game2D.py
+++ Plane_Game2D.py
@@ -13,16 +13,16 @@ Kills = 0
 WIDTH = 500
 HEIGHT = 700
 
-#创建Player
+# create Player
 player = Actor('player_plane')
 player.x = 250
 player.y = 650
 
 weapons = []
-#创建3个enemy角色
+# create three enemy entity
 enemy = []
 for i in range(3):
-    e = Actor('bubu')
+    e = Actor('en')
     e.x = random.randint(0, 500)
     enemy.append(e)
 
@@ -31,24 +31,24 @@ def draw():
     global cooldown
     global Kills
     global time
-    screen.blit('星球', [0, 0])
+    screen.blit('bg', [0, 0])
     player.draw()
-    #游戏失败之前绘制角色
-    if player.image != '失败':
+    # fucked char
+    if player.image != 'fail':
         for w in weapons:
             w.draw()
         for e in enemy:
             e.draw()
-#空格键发射子弹   
+# shoot
 def on_key_down(key):
     global counter
     global cooldown
     global Kills
     global time
-    #鼠标左键发射
+    # left click for shoot
     control = mouse.Controller()
     if keyboard.space:
-        weapon = Actor('子弹')
+        weapon = Actor('aml')
         weapon.x = player.x
         weapon.y = player.y - 50
         weapons.append(weapon)
@@ -57,7 +57,7 @@ def on_mouse_down():
     global cooldown
     global Kills
     global time
-    weapon = Actor('子弹')
+    weapon = Actor('aml')
     weapon.x = player.x
     weapon.y = player.y - 50        
     weapons.append(weapon)
@@ -69,7 +69,7 @@ def update():
     global Kills
     global time
     counter += 1
-    #Player移动
+    #Player move
     if keyboard.left or keyboard.a:
         player.x -= 5
     if keyboard.right or keyboard.d:
@@ -78,16 +78,16 @@ def update():
         player.y -= 5
     if keyboard.down or keyboard.s:
         player.y += 5 
-    if player.image == '失败':
+    if player.image == 'fail':
         print('You Have Killed',Kills)
         print('You Got in',time,'secounds')
         exit()
-    #计时
+    # timer
     for e in enemy:
         if counter == 60:
             counter = 0
             time += 1
-    #让enemy随机走动
+    # make entity movwe
     if e.y >= 150:
         random_num = random.randint(1, 5)
         if random == 1:
@@ -96,21 +96,21 @@ def update():
             e.x -= 2
 
     
-    #子弹从下向上移动, 到窗口上边缘消失    
+    # aim
     for w in weapons:
         w.y -= 30
     for w in weapons:
         if w.y <= 0:
             weapons.remove(w)
     
-    # enemy从上向下移动, 到窗口下边后初始化到窗口上方
+    # enemy fix
     for e in enemy:
         e.y += 5
         if e.y > 700:
             e.x = random.randint(0, 500)
             e.y = 0
     
-    #enemy和子弹碰撞后, 回到初始位置
+    #enemy init
     for e in enemy:
         for w in weapons:
             if e.colliderect(w):
@@ -120,13 +120,13 @@ def update():
                 weapons.remove(w)
 
         
-        #enemy碰到Player游戏失败
+        # faild
         if e.colliderect(player) or player.y <= -15 or player.y >= 715 or player.x >= 515 or player.x <= -15:
-            player.image = '失败'
+            player.image = 'fail'
             #播放失败音乐
             music.play('faild')
 
-#播放背景音乐
+# background music
 music.play('music1')
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
